60072a68620e4f535e3d7367492305554ebee2eb
Devonte Durham <durhamd@fvtc.edu>
2020-09-16 17:14:59 -0500

    Added empty managers in the BL.

--- GroceryGetter.BL/UserProductManager.cs
+++ GroceryGetter.BL/UserProductManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GroceryGetter.BL
 {
-    public class Class1
+    public class UserProductManager
     {
     }
 }
--- GroceryGetter.UI/Controllers/HomeController.cs
+++ GroceryGetter.UI/Controllers/HomeController.cs
@@ -15,15 +15,12 @@ namespace GroceryGetter.UI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            
             return View();
         }
     }
