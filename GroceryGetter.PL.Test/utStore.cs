7e3c7b32f93d84dc023816f0188a42fc0fd6e11a
Devonte Durham <durhamd@fvtc.edu>
2020-09-19 17:15:08 -0500

    Added edmx and published database to Azure. Added one test to make sure everything works.

---
+++ GroceryGetter.PL.Test/utStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryGetter.PL;
+using System.Linq;
+
+namespace GroceryGetter.PL.Test
+{
+    [TestClass]
+    public class utStore
+    {
+        [TestMethod]
+        public void Insert()
+        {
+            try
+            {
+                using (GroceryGetterEntities dc = new GroceryGetterEntities())
+                {
+                    Assert.AreEqual(3, dc.tblStores.Count());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            
+        }
+    }
+}
