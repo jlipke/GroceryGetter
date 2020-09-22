fad99cc3f7b738ae4ca74dbbc7e1d04c096cc8a4
J Lipke <l.jlipke@gmail.com>
2020-09-21 21:43:53 -0500

    Installed Entity Framework

--- GroceryGetter.BL/AisleProductManager.cs
+++ GroceryGetter.BL/AisleProductManager.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GroceryGetter.PL;
+using GroceryGetter.BL.Models;
 
 namespace GroceryGetter.BL
 {
     public class AisleProductManager
     {
+        public static int Insert(AisleProduct aisleProduct)
+        {
+            try
+            {
+                using (GroceryGetterEntities dc = new GroceryGetterEntities())
+                {
+                    tblAisleProduct ap = new tblAisleProduct();
+                    ap.Id = Guid.NewGuid();
+                    ap.AisleId = aisleProduct.AisleId;
+                    ap.ProductId = aisleProduct.ProductId;
+
+                    dc.tblAisleProducts.Add(ap);
+
+                    return dc.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
