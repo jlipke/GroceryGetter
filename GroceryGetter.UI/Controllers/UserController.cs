5c496b833b552c612e581ee4351f869791687369
Matther Butcher <300019014@fvtc.edu>
2020-10-02 13:25:30 -0500

    Coded UserController, added Create & Login views, modified Site.css and _Layout.cshtml
    Related Work Items: #98, #106

--- GroceryGetter.UI/Controllers/UserController.cs
+++ GroceryGetter.UI/Controllers/UserController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GroceryGetter.BL;
+using GroceryGetter.BL.Models;
 
 namespace GroceryGetter.UI.Controllers
 {
     public class UserController : Controller
     {
-        // GET
+        // GET: User
         public ActionResult Index()
         {
-            return View();
+            // Authenticate.cs needs to be added to the Models folder (DVDCentral)
+            /*if (Authenticate.IsAuthenticated())
+            {
+                List<User> users = UserManager.Load();
+                return View(users);
+            }
+            else*/
+            {
+                //Need to authenticate
+                return RedirectToAction("Login", "User", new { returnurl = HttpContext.Request.Url });
+            }
+
         }
 
         // GET
@@ -20,20 +33,59 @@ namespace GroceryGetter.UI.Controllers
             return View();
         }
 
-        // GET
-        public ActionResult SignUp()
+        // GET User/Login
+        public ActionResult Login(string returnurl)
         {
-            return View();
+            User user = new User();
+            ViewBag.ReturnUrl = returnurl;
+            return View(user);
         }
 
-        // POST
+        //POST User/Login
         [HttpPost]
-        public ActionResult SignUp(FormCollection collection)
+        public ActionResult Login(User user, string returnurl)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (UserManager.Login(user))
+                {
+                    Session["user"] = user;
+                    if (!String.IsNullOrEmpty(returnurl))
+                        return Redirect(returnurl);
+                    else
+                        return RedirectToAction("Index", "User");
+                }
+                ViewBag.Message = "Login could not be completed.";
+                return View(user);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(user);
+            }
+        }
 
+        public ActionResult Logout()
+        {
+            Session["user"] = null;
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            User user = new User();
+            ViewBag.Title = "Create User";
+            return View(user);
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                UserManager.Insert(user);
                 return RedirectToAction("Index");
             }
             catch
