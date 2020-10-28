23f2594684fc8a5eaf2cc3960d16779ba37002f6
Devonte Durham <durhamd@fvtc.edu>
2020-10-28 17:08:26 -0500

    Rmoved changes from controllers

--- GroceryGetter.UI/Controllers/UserProductController.cs
+++ GroceryGetter.UI/Controllers/UserProductController.cs
@@ -18,10 +18,8 @@ namespace GroceryGetter.UI.Controllers
             if (Authenticate.IsAuthenticated())
             {
                 var user = Session["user"] as User;
-                //var userProductList = UserProductManager.LoadByUserId(user.Id);
-                //var userProductList = new List<UserProduct>(); 
-                //ViewBag.Message = ViewBag.Message;
-                return View(user);
+                var userProductList = UserProductManager.LoadByUserId(user.Id);
+                return View(userProductList);
             }
             else
             {
@@ -31,35 +29,7 @@ namespace GroceryGetter.UI.Controllers
             
         }
 
-        // GET
-        [HttpPost]
-        public ActionResult Index(Product p)
-        {
-            if (Authenticate.IsAuthenticated())
-            {
-                var x = HttpContext.Request.Form["ItemName"];
-                var user = Session["user"] as User;
-
-                UserProduct up = new UserProduct();
-                up.Aisle = p.Aisle;
-                up.Id = p.Id;
-                up.Store = p.Store;
-                //up.Title = p.Title;
-                up.Title = x;
-
-                ProductHelper.AddItemToList(up, user.GroceryListObj);
-                ViewBag.Message = x;
-                return View(user);
-            }
-            else
-            {
-                //Need to authenticate
-                return RedirectToAction("Login", "User", new { returnurl = HttpContext.Request.Url });
-            }
-
-        }
-
-
+      
         // GET
         public ActionResult Details(int id)
         {
@@ -71,10 +41,7 @@ namespace GroceryGetter.UI.Controllers
         {
             if (Authenticate.IsAuthenticated())
             {
-                UpProducts upProducts = new UpProducts();
-                upProducts.UserProduct = new UserProduct();
-                upProducts.Products = ProductManager.LoadAll();
-                return View(upProducts);
+                return View();
             }
 
             return View();
@@ -83,12 +50,12 @@ namespace GroceryGetter.UI.Controllers
 
         // POST
         [HttpPost]
-        public ActionResult Create(UpProducts upProducts)
+        public ActionResult Create(FormCollection collection)
         {
             try
             {
                 // TODO: Add insert logic here
-                //UserProductManager.Insert(upProducts.UserProduct);
+                
                 return RedirectToAction("Index");
             }
             catch
@@ -98,25 +65,19 @@ namespace GroceryGetter.UI.Controllers
         }
 
         // GET
-        public ActionResult Edit(Product product)
+        public ActionResult Edit(FormCollection collection)
         {
-            var user = Session["user"] as User;
-            //UpProducts upProducts = new UpProducts();
-            //upProducts.UserProduct = UserProductManager.SearchGroceryByProduct(user.Id, userProduct.ProductTitle);
-            //UserProduct userProduct = UserProductManager.SearchGroceryByProduct(user.Id, product.Title);
-
-
-            var userProduct = new List<UserProduct>();
-            return View(userProduct);
+            
+            return View();
         }
 
         // POST
         [HttpPost]
-        public ActionResult Edit(UserProduct userProduct)
+        public ActionResult Edit(int id, FormCollection collection)
         {
             try
             {
-               // UserProductManager.Update(userProduct);
+               
                 return RedirectToAction("Index");
             }
             catch
@@ -125,31 +86,6 @@ namespace GroceryGetter.UI.Controllers
             }
         }
 
-
-        //// GET
-        //public ActionResult Edit(Guid id)
-        //{
-        //    UserProduct userProduct = UserProductManager.LoadById(id);
-        //    //UpProducts upProducts = new UpProducts();
-        //    //upProducts.UserProduct = new UserProduct();
-        //    //upProducts.Products = ProductManager.LoadAll();
-        //    return View(userProduct);
-        //}
-
-        //// POST
-        //[HttpPost]
-        //public ActionResult Edit(UpProducts upProducts, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        UserProductManager.Update(upProducts.UserProduct);
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
 
 
         // GET
@@ -173,9 +109,6 @@ namespace GroceryGetter.UI.Controllers
                 return View();
             }
         }
-
-
-        
 
     }
 }
