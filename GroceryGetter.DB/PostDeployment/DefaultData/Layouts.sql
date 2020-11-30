5c479b6f160db8aab17c277600f08ba55c813551
Matther Butcher <300019014@fvtc.edu>
2020-11-30 10:49:29 -0600

    Deleted Alphabetical layout from the Layouts.sql to fix database publish

--- GroceryGetter.DB/PostDeployment/DefaultData/Layouts.sql
+++ GroceryGetter.DB/PostDeployment/DefaultData/Layouts.sql
@@ -1,12 +1,5 @@
 ﻿BEGIN
 	DECLARE @StoreId uniqueidentifier;
-	
-
-	-- ** Create the Title in the UI by concatenating two textboxes "tblStore.Title = txtStoreName.Text + "-" + txtStoreCity/txtStoreLocation"
-	SELECT @StoreId = Id from tblStore where Title = 'Alphabetical'
-	INSERT INTO dbo.tblLayout(Id, StoreId, Title)
-	VALUES
-	(NEWID(), @StoreId, 'L_Alphabetical-A-Z')
 
 	-- -De Pere in Title is the unique location identifier. 
 	SELECT @StoreId = Id from tblStore where Title = 'Festival-De Pere'
